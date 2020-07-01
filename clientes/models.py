12f4040924c0c886f59415e1f764305399c41415
EricksonLopes <ofc.erickson@gmail.com>
2020-07-01 12:07:27 -0300

    models

---
+++ clientes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    salary = models.DecimalField(decimal_places=2, max_digits=5)
+    bio = models.TextField()
