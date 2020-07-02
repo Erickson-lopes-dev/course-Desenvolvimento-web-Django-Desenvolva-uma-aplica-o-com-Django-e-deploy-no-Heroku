364f6a95b9511084c3647b357ccf18febb70ea20
EricksonLopes <ofc.erickson@gmail.com>
2020-07-02 08:33:17 -0300

    viewing media files

--- meuPrimeiroProjeto/urls.py
+++ meuPrimeiroProjeto/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello),
     path('articles/<int:year>', articles),
     # path('pesquisa/<str:name>', fname),
     path('pesquisa/<str:name>', fname2),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
