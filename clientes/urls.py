5e8f002cc3a0cd53d1b24662f5d5f75d982e43b8
EricksonLopes <ofc.erickson@gmail.com>
2020-07-02 09:58:03 -0300

    CRUD - create

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('list/', person_list),
+    path('list/', person_list, name='person_list'),
+    path('new/', person_new, name='person_new'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Person
+from .forms import PersonForm
 
 
 def person_list(request):
     persons = Person.objects.all()
     return render(request, 'person.html', {'persons': persons})
+
+
+def person_new(request):
+    # modo de enviar o formulário, e arquivos de media
+    form = PersonForm(request.POST, None, request.FILES)
+
+    # validando fomulário
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+    # retorna o template e envia o form ao html
+    return render(request, 'person_form.html', {'form': form})
